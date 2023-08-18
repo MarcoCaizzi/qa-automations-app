96a27f19262f2aaa677de87deca9479c9b59a90c
Marco Caizzi <marcocaizzi@hotmail.com>
2023-08-17 21:51:20 -0300

    base app

--- lib/main.dart
+++ lib/main.dart
@@ -5,79 +5,56 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Uala Testing',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const FirstScreen(),
-      routes: {
-        '/second': (context) => const SecondScreen(),
-      },
+    return const MaterialApp(
+      home: DataEntryScreen(),
     );
   }
 }
 
-class FirstScreen extends StatefulWidget {
-  const FirstScreen({Key? key}) : super(key: key);
-
-  @override
-  State<FirstScreen> createState() => _FirstScreenState();
-}
-
-class _FirstScreenState extends State<FirstScreen> {
-  final nameController = TextEditingController();
-  final lastNameController = TextEditingController();
-  final birthDateController = TextEditingController();
-
-  @override
-  void dispose() {
-    nameController.dispose();
-    lastNameController.dispose();
-    birthDateController.dispose();
-    super.dispose();
-  }
+class DataEntryScreen extends StatelessWidget {
+  const DataEntryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Uala Testing'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      appBar: AppBar(title: const Text('Ingreso de Datos')),
+      body: const DataEntryForm(),
+    );
+  }
+}
+
+class DataDisplayScreen extends StatelessWidget {
+  final String nombre;
+  final String apellido;
+  final String fechaNacimiento;
+  final String email;
+  final String telefono;
+
+  DataDisplayScreen({super.key,
+    required this.nombre,
+    required this.apellido,
+    required this.fechaNacimiento,
+    required this.email,
+    required this.telefono,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Datos Ingresados')),
+      body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              'Ingrese sus datos',
-              style: TextStyle(fontSize: 24),
-            ),
-            TextField(
-              controller: nameController,
-              decoration: const InputDecoration(labelText: 'Nombre'),
-            ),
-            TextField(
-              controller: lastNameController,
-              decoration: const InputDecoration(labelText: 'Apellido'),
-            ),
-            TextField(
-              controller: birthDateController,
-              decoration: const InputDecoration(labelText: 'Fecha de nacimiento'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/second', arguments: {
-                  'name': nameController.text,
-                  'lastName': lastNameController.text,
-                  'birthDate': birthDateController.text,
-                });
-              },
-              child: const Text('Ingresar'),
-            ),
+            Text('Nombre: $nombre $apellido'),
+            Text('Fecha de Nacimiento: $fechaNacimiento'),
+            Text('Email: $email'),
+            Text('Teléfono: $telefono'),
           ],
         ),
       ),
@@ -85,27 +62,125 @@ class _FirstScreenState extends State<FirstScreen> {
   }
 }
 
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({Key? key}) : super(key: key);
+class DataEntryForm extends StatefulWidget {
+  const DataEntryForm({super.key});
+
+  @override
+  _DataEntryFormState createState() => _DataEntryFormState();
+}
+
+class _DataEntryFormState extends State<DataEntryForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _nombre = '';
+  String _apellido = '';
+  String _fechaNacimiento = '';
+  String _email = '';
+  String _telefono = '';
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => DataDisplayScreen(
+            nombre: _nombre,
+            apellido: _apellido,
+            fechaNacimiento: _fechaNacimiento,
+            email: _email,
+            telefono: _telefono,
+          ),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final args = ModalRoute.of(context)!.settings.arguments as Map<String, String>;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Demo'),
-      ),
-      body: Center(
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Hola ${args['name']} ${args['lastName']}',
-              style: const TextStyle(fontSize: 24),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Nombre'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor ingresa tu nombre';
+                }
+                return null;
+              },
+              onSaved: (value) => _nombre = value!,
             ),
-            Text(
-              'Tu fecha de nacimiento es ${args['birthDate']}',
-              style: const TextStyle(fontSize: 24),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Apellido'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor ingresa tu apellido';
+                }
+                return null;
+              },
+              onSaved: (value) => _apellido = value!,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Fecha de Nacimiento'),
+              onTap: () async {
+                FocusScope.of(context).requestFocus(FocusNode());
+                final selectedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(1900),
+                  lastDate: DateTime.now(),
+                );
+                if (selectedDate != null) {
+                  setState(() {
+                    _fechaNacimiento =
+                    selectedDate.toLocal().toString().split(' ')[0];
+                  });
+                }
+              },
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor ingresa tu fecha de nacimiento';
+                }
+                return null;
+              },
+              readOnly: true,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor ingresa tu email';
+                }
+                if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+                  return 'Por favor ingresa un email válido';
+                }
+                return null;
+              },
+              onSaved: (value) => _email = value!,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Teléfono'),
+              keyboardType: TextInputType.phone,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor ingresa tu número de teléfono';
+                }
+                if (!RegExp(r'^[0-9]{10}$').hasMatch(value)) {
+                  return 'Por favor ingresa un número de teléfono válido';
+                }
+                return null;
+              },
+              onSaved: (value) => _telefono = value!,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _submitForm,
+              child: const Text('Ingresar'),
             ),
           ],
         ),
